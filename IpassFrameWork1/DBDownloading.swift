2517fe5d52e90cda0257a1d4827e4c4a104130e2
Raman Rana <appsdev096@gmail.com>
2024-04-10 12:50:15 +0530

    first commit with podspec

---
+++ IpassFrameWork1/DBDownloading.swift
@@ -0,0 +1,18 @@
+//
+//  DBDownloading.swift
+//  IpassFrameWork1
+//
+//  Created by Mobile on 10/04/24.
+//
+
+import Foundation
+
+public class DataBaseDownloading{
+    
+    public static func addToNumbers(a:Int, b:Int)-> Int{
+       return a + b
+    }
+    public static func minusToNumbers(a:Int, b:Int)-> Int{
+       return a - b
+    }
+}
